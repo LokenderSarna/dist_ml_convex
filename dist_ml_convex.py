9ae9782965eae42a4196152140273b6e6bbfac9b
Michael Curtis <michaelcurtis@Michaels-MacBook-Pro.local>
2015-05-25 13:32:20 -0400

    Basic gradient descent execution working for 1d example to find the minimum

--- dist_ml_convex.py
+++ dist_ml_convex.py
@@ -13,14 +13,28 @@ class convex_problem:
             self.x0 = np.zeros(d)
         else:
             self.x0 = x0
-
+                    
 # Defaults for gradient descent parameters
-EPSILON_DEFAULT = 0.01
-MAX_ITER_DEFAULT = 100000
+EPSILON_DEFAULT = 0.1
+MAX_ITER_DEFAULT = 1000
+ALPHA_DEFAULT = 0.01
 
-# Since alpha will be a function our default will also be a function to match type checking
-def ALPHA_DEFAULT(x):
-     return 0.5
+# A method to find the p=2 norm of a set of lambda function
+# NOTE: can we have the lambda take input an array?????
+# Is this saying that each fi(x) has d dimensions?????
+def l2norm(fi_x):
+    
+    if isinstance(fi_x, np.int64):
+        return np.absolute(fi_x)
+
+    if isinstance(fi_x, int):
+        return np.absolute(fi_x)
+
+    else:
+        sum = 0
+        for dimension in fi_x:
+            sum += math.pow(dimension, 2)
+        return math.sqrt(sum)
 
 # Gradient_descent will take a convex problem as input and find the optimal x* 
 class gradient_descent:
@@ -32,8 +46,11 @@ class gradient_descent:
         self.n = convex_problem.n
         self.lambdas = convex_problem.lambdas
         self.x = convex_problem.x0
+        # Since alpha will be a function our default will also be a function to match type checking
         if alpha is None:
-            self.alpha = ALPHA_DEFAULT(0)
+            alpha_def = np.zeros(self.d)
+            alpha_def.fill(ALPHA_DEFAULT)
+            self.alpha = (lambda x : alpha_def)
         else:
             self.alpha = alpha
         if epsilon is None:
@@ -44,21 +61,33 @@ class gradient_descent:
             self.max_iter = MAX_ITER_DEFAULT
         else:
             self.max_iter = max_iter
-    
+                
     # Here we will actually execute the algorithm
     def execute(self):
         
-        # While the l2 norm of the gradient of the vector x is less then epsilon (the stopping criterion), keep going
-        
-    
-    # A method to find the p=2 norm of a set of lambda function
-    # NOTE: can we have the lambda take input an array?????
-    def l2norm(self, x, lambdas):
-        sum = 0
-        for fi in lambdas:
-            sum += math.pow(fi(x), 2)
-        return math.sqrt(sum)
-            
+        #for each fi function we need to minimize such that the fi (gradient function) is within epsilon
+        for fi in self.lambdas:
+                        
+            # Iterate through until the x vector values allow this fi to fall within epsilon
+            inside_epsilon = False
+            num_iter = 0
+            while inside_epsilon == False:
+                
+                num_iter += 1
+                # If we can't find a value within the max iteration limit return None
+                if num_iter > self.max_iter:
+                    return None
+
+                # Here we do the update if we aren't within the epsilon value
+                fi_x = fi(self.x)
+                print l2norm(fi_x)
+                if l2norm(fi_x) > self.epsilon:
+                    # Note that fi is a function that goes from fi: R^d -> R^d, xk+1 = xk = alpha(xk)*f(xk)
+                    self.x = self.x - self.alpha(self.x)*fi_x
+                    print "grad @ fi_x: %f, x: %f" %(fi_x, self.x)
+                else:
+                    inside_epsilon = True
+                    print "DONE!"
         
         
         
--- test_script.py
+++ test_script.py
@@ -1,7 +1,8 @@
 from dist_ml_convex import *
+import numpy as np
 
-lambdas = [lambda x : x + 2, lambda x : x + 5]
-prob = convex_problem(3, 2, lambdas, 4)
+lambdas = [lambda x : 2*x +6]
+prob = convex_problem(1, 1, lambdas, lambdas[0](2))
 
 grad = gradient_descent(prob)
 grad.execute()
