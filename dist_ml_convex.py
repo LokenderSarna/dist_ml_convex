f9580fe1c7913af868cc03ad9d06237bf690fc2e
Michael Curtis <michaelcurtis@Michaels-MacBook-Pro.local>
2015-05-26 14:09:23 -0400

    Gradient descent now working for quadratic test case

--- dist_ml_convex.py
+++ dist_ml_convex.py
@@ -2,42 +2,25 @@ import numpy as np
 import math
 
 # Here we define the class convex_problem to build a convex optimization problem to be sent to gradient descent
-class convex_problem:
+class ConvexProblem:
 	
     # Take input basic parameters; d: x num vector dimensions, n: num terms in sum, lambdas: functions fis, x0: initial x value 
-    def __init__(self, d, n, lambdas, x0=None):
+    def __init__(self, d, lambdas, x0=None):
         self.d = d
-        self.n = n
+        self.n = len(lambdas)
         self.lambdas = lambdas
         if x0 is None:
-            self.x0 = np.zeros(d)
+            self.x0 = np.random.random((self.d,1))
         else:
             self.x0 = x0
                     
 # Defaults for gradient descent parameters
-EPSILON_DEFAULT = 0.1
-MAX_ITER_DEFAULT = 1000
-ALPHA_DEFAULT = 0.01
-
-# A method to find the p=2 norm of a set of lambda function
-# NOTE: can we have the lambda take input an array?????
-# Is this saying that each fi(x) has d dimensions?????
-def l2norm(fi_x):
-    
-    if isinstance(fi_x, np.int64):
-        return np.absolute(fi_x)
-
-    if isinstance(fi_x, int):
-        return np.absolute(fi_x)
-
-    else:
-        sum = 0
-        for dimension in fi_x:
-            sum += math.pow(dimension, 2)
-        return math.sqrt(sum)
+EPSILON_DEFAULT = 0.001
+MAX_ITER_DEFAULT = 10000000
+ALPHA_DEFAULT = 0.001
 
 # Gradient_descent will take a convex problem as input and find the optimal x* 
-class gradient_descent:
+class GradientDescent:
     
     # Parameters taken as input; alpha: the step size function, epsilon the stopping critera, epsilon: the stop criterion, max_iter: upper limit on the number of iterations
     def __init__(self, convex_problem, alpha=None, epsilon=None, max_iter=None):
@@ -48,9 +31,7 @@ class gradient_descent:
         self.x = convex_problem.x0
         # Since alpha will be a function our default will also be a function to match type checking
         if alpha is None:
-            alpha_def = np.zeros(self.d)
-            alpha_def.fill(ALPHA_DEFAULT)
-            self.alpha = (lambda x : alpha_def)
+            self.alpha = (lambda x : ALPHA_DEFAULT)
         else:
             self.alpha = alpha
         if epsilon is None:
@@ -64,30 +45,32 @@ class gradient_descent:
                 
     # Here we will actually execute the algorithm
     def execute(self):
+                      
+        # Iterate through until the x vector values allow the summed up fis to fall within epsilon
+        cont_iter = True
+        num_iter = 0
         
-        #for each fi function we need to minimize such that the fi (gradient function) is within epsilon
-        for fi in self.lambdas:
-                        
-            # Iterate through until the x vector values allow this fi to fall within epsilon
-            inside_epsilon = False
-            num_iter = 0
-            while inside_epsilon == False:
-                
-                num_iter += 1
-                # If we can't find a value within the max iteration limit return None
-                if num_iter > self.max_iter:
-                    return None
-
-                # Here we do the update if we aren't within the epsilon value
-                fi_x = fi(self.x)
-                print l2norm(fi_x)
-                if l2norm(fi_x) > self.epsilon:
-                    # Note that fi is a function that goes from fi: R^d -> R^d, xk+1 = xk = alpha(xk)*f(xk)
-                    self.x = self.x - self.alpha(self.x)*fi_x
-                    print "grad @ fi_x: %f, x: %f" %(fi_x, self.x)
-                else:
-                    inside_epsilon = True
-                    print "DONE!"
-        
-        
-        
+        while cont_iter == True:
+            
+            # Increment the number of iterations, also note alpha is a function of num_iter
+            num_iter += 1
+            
+            # Where g_x is the summation of all of our fi's at each x
+            g_x = np.ndarray(shape=(self.d,1))
+            g_x.fill(0)
+            for fi in self.lambdas:
+                g_x = np.add(g_x, fi(self.x))
+            
+            # Here we do the update if we aren't within the epsilon value
+            if np.linalg.norm(g_x) > self.epsilon:
+                # Note that fi is a function that goes from fi: R^d -> R^d, xk+1 = xk = alpha(num_iter)*f(xk)
+                self.x = np.subtract(self.x, np.multiply(self.alpha(num_iter),g_x))
+            else:
+                cont_iter = False
+                print "Gradient descent has found a solution:"
+                print self.x
+            
+            # If we can't find a value within the max iteration limit return None
+            if num_iter > self.max_iter:
+                print "Stopping after reaching iteration limit."
+                cont_iter = False
--- test_script.py
+++ test_script.py
@@ -1,8 +1,33 @@
 from dist_ml_convex import *
 import numpy as np
 
-lambdas = [lambda x : 2*x +6]
-prob = convex_problem(1, 1, lambdas, lambdas[0](2))
+#lambdas = [lambda x : 2*x +6]
+#prob = ConvexProblem(1, 1, lambdas, lambdas[0](2))
 
-grad = gradient_descent(prob)
+Q1 = np.random.random((5,5))
+Q1 = 0.5*(Q1 + np.transpose(Q1))
+Q1 = np.linalg.matrix_power(Q1, 2)
+Q2 = np.random.random((5,5))
+Q2 = 0.5*(Q2 + np.transpose(Q2))
+Q2 = np.linalg.matrix_power(Q2, 2)
+Q3 = np.random.random((5,5))
+Q3 = 0.5*(Q3 + np.transpose(Q3))
+Q3 = np.linalg.matrix_power(Q3, 2)
+
+q1 = np.random.random((5,1))
+q2 = np.random.random((5,1))
+q3 = np.random.random((5,1))
+
+lambdas = [ lambda x : np.dot(Q1, x) + q1, lambda x : np.dot(Q2, x) + q2, lambda x : np.dot(Q3, x) + q3 ]
+
+prob = ConvexProblem(5, lambdas)
+
+grad = GradientDescent(prob)
+
+Q = Q1 + Q2 + Q3
+q = q1 + q2 + q3
+
 grad.execute()
+
+print "Closed form Solution: "
+print - np.dot(np.linalg.inv(Q), q)
