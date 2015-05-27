d5c35859ac4a733ad4b00daebfeb23a7c4db1068
Michael Curtis <michaelcurtis@Michaels-MacBook-Pro.local>
2015-05-27 11:34:32 -0400

    Removed ConvexProblem class and added FirstOrderOracle

--- dist_ml_convex.py
+++ dist_ml_convex.py
@@ -4,25 +4,18 @@ import math
 # Defining a layer of abstraction for each fi. Which will likely end up being a cut where each fi is responsible for some subset of data a machine learning algorithm needs to sift through
 class FirstOrderOracle:
 
-    # Where lambdas are the fis and grad lambdas are their gradients
-    def __init__(self, lambdas, grad_lambdas):
-        self.lambdas = lambdas
-        self.grad_lambdas = grad_lambdas
-
-# Here we define the class convex_problem to build a convex optimization problem to be sent to gradient descent
-class ConvexProblem:
-	
-    # Take input basic parameters; d: x num vector dimensions, n: num terms in sum, lambdas: functions fis, x0: initial x value 
-    def __init__(self, d, lambdas, x0=None):
-        self.d = d
-        self.lambdas = lambdas
-        if x0 is None:
-            self.x0 = np.random.random((self.d,1))
+    # Where f is the function and grad_f is its gradient
+    def __init__(self, f, grad_f, f_dim, x_init=None):
+        self.f = f
+        self.grad_f = grad_f
+        self.f_dim = f_dim
+        if x_init == None:
+            self.x_init = np.random.random((self.f_dim, 1))
         else:
-            self.x0 = x0
-                    
+            self.x_init = x_init
+     
 # Defaults for gradient descent parameters
-EPSILON_DEFAULT = 0.001
+EPSILON_DEFAULT = 0.0001
 MAX_ITER_DEFAULT = 10000000
 ALPHA_DEFAULT = 0.001
 
@@ -30,11 +23,10 @@ ALPHA_DEFAULT = 0.001
 class GradientDescent:
     
     # Parameters taken as input; alpha: the step size function, epsilon the stopping critera, epsilon: the stop criterion, max_iter: upper limit on the number of iterations
-    def __init__(self, convex_problem, alpha=None, epsilon=None, max_iter=None):
-        self.convex_problem = convex_problem
-        self.d = convex_problem.d
-        self.lambdas = convex_problem.lambdas
-        self.x = convex_problem.x0
+    def __init__(self, oracles, alpha=None, epsilon=None, max_iter=None):
+        self.oracles = oracles
+        self.f_dim = self.oracles[0].f_dim
+        self.x = self.oracles[0].x_init
         # Since alpha will be a function our default will also be a function to match type checking
         if alpha is None:
             self.alpha = (lambda x : ALPHA_DEFAULT)
@@ -48,8 +40,8 @@ class GradientDescent:
             self.max_iter = MAX_ITER_DEFAULT
         else:
             self.max_iter = max_iter
-                
-    # Here we will actually execute the algorithm
+            
+    # Here we will actually execute the algorithm and return the solution if one is reached
     def execute(self):
                       
         # Iterate through until the x vector values allow the summed up fis to fall within epsilon
@@ -62,10 +54,10 @@ class GradientDescent:
             num_iter += 1
             
             # Where g_x is the summation of all of our fi's at each x
-            g_x = np.ndarray(shape=(self.d,1))
+            g_x = np.ndarray(shape=(self.f_dim,1))
             g_x.fill(0)
-            for fi in self.lambdas:
-                g_x = np.add(g_x, fi(self.x))
+            for oracle in self.oracles:
+                g_x = np.add(g_x, oracle.grad_f(self.x))
             
             # Here we do the update if we aren't within the epsilon value
             if np.linalg.norm(g_x) > self.epsilon:
@@ -75,8 +67,10 @@ class GradientDescent:
                 cont_iter = False
                 print "Gradient descent has found a solution:"
                 print self.x
+                return self.x
             
             # If we can't find a value within the max iteration limit return None
             if num_iter > self.max_iter:
                 print "Stopping after reaching iteration limit."
                 cont_iter = False
+                #NOTE: add a error message for return or some flag
--- test_script.py
+++ test_script.py
@@ -1,33 +1,40 @@
 from dist_ml_convex import *
 import numpy as np
 
-#lambdas = [lambda x : 2*x +6]
-#prob = ConvexProblem(1, 1, lambdas, lambdas[0](2))
-
-Q1 = np.random.random((5,5))
+Q1 = np.random.random((10,10))
 Q1 = 0.5*(Q1 + np.transpose(Q1))
 Q1 = np.linalg.matrix_power(Q1, 2)
-Q2 = np.random.random((5,5))
+Q2 = np.random.random((10,10))
 Q2 = 0.5*(Q2 + np.transpose(Q2))
 Q2 = np.linalg.matrix_power(Q2, 2)
-Q3 = np.random.random((5,5))
+Q3 = np.random.random((10,10))
 Q3 = 0.5*(Q3 + np.transpose(Q3))
 Q3 = np.linalg.matrix_power(Q3, 2)
 
-q1 = np.random.random((5,1))
-q2 = np.random.random((5,1))
-q3 = np.random.random((5,1))
-
-lambdas = [ lambda x : np.dot(Q1, x) + q1, lambda x : np.dot(Q2, x) + q2, lambda x : np.dot(Q3, x) + q3 ]
-
-prob = ConvexProblem(5, lambdas)
-
-grad = GradientDescent(prob)
+q1 = np.random.random((10,1))
+q2 = np.random.random((10,1))
+q3 = np.random.random((10,1))
 
 Q = Q1 + Q2 + Q3
 q = q1 + q2 + q3
 
-grad.execute()
-
 print "Closed form Solution: "
 print - np.dot(np.linalg.inv(Q), q)
+
+f1 = lambda x : (1/2)*(np.dot(np.dot(np.transpose(x), Q1), x)) + np.dot(q1, x)
+f2 = lambda x : (1/2)*(np.dot(np.dot(np.transpose(x), Q2), x)) + np.dot(q2, x)
+f3 = lambda x : (1/2)*(np.dot(np.dot(np.transpose(x), Q3), x)) + np.dot(q3, x)
+
+grad_f1 = lambda x : np.dot(Q1, x) + q1
+grad_f2 = lambda x : np.dot(Q2, x) + q2
+grad_f3 = lambda x : np.dot(Q3, x) + q3
+
+oracle1 = FirstOrderOracle(f1, grad_f1, 10) 
+oracle2 = FirstOrderOracle(f2, grad_f2, 10) 
+oracle3 = FirstOrderOracle(f3, grad_f3, 10) 
+
+oracles = [oracle1, oracle2, oracle3]
+
+grad = GradientDescent(oracles)
+
+grad.execute()
